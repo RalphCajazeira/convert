a8ef9bd67e7625b033b4e6074fdc5baf568f59d7
Ralph Ramos <ralphmtk@gmail.com>
2025-01-16 11:37:40 -0300

    Finalizado

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// Cotação de moedas do dia.
+// Cotação de moedas do dia, valores em centavos, isso ajuda a deixar com mais precisão os valores.
 const USD = 487;
 const EUR = 532;
 const GBP = 608;
@@ -49,9 +49,16 @@ function convertCurrency(amount, price, symbol) {
     description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`;
 
     // Calcula a converção da moeda para Reais
-    result.textContent = `${formatCurrencyBRL(amount * price)} Reais`;
+    let total = amount * price;
+
+    // Converter o total para o modelo de R$ usando a função, remover o R$ com replace e o trim para remover espaços
+    total = formatCurrencyBRL(total).replace("R$", "").trim();
+
+    // Substitui o texto no footer do H1 que tem a informação do valor Total da conversão
+    result.textContent = `${total} Reais`;
   } catch (error) {
     console.log(error);
+
     // Remove a classe do footer para remover ele da tela.
     footer.classList.remove("show-result");
     alert("Não foi possivel converter. Tente novamente mais tarde.");
@@ -60,6 +67,7 @@ function convertCurrency(amount, price, symbol) {
 
 // Função para formatar para moeda R$ (Real).
 function formatCurrencyBRL(value) {
+  // Converter para número para ultilizar o toLocaleString para formatar no padrão BRL (R$ 0,00)
   return Number(value / 100).toLocaleString("pt-BR", {
     style: "currency",
     currency: "BRL",
